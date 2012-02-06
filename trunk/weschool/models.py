212a7ed295729b21d238218659d8092f0ca0a06e
orn@skynvilla.is <orn@skynvilla.is@941bb81a-f064-d633-28c5-ba8e112e393c>
2012-02-06 22:10:34 +0000

    __unicode__ bætt við

--- trunk/weschool/models.py
+++ trunk/weschool/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class Exam(models.Model):
     question = models.CharField(max_length=100)
 
+    def __unicode__(self):
+        return self.question
+
 class Teacher(models.Model):
     name = models.CharField(max_length=100)
     course = models.ForeignKey('Course')
@@ -12,8 +15,17 @@ class Teacher(models.Model):
     )
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
 
+    def __unicode__(self):
+        return self.name
+
 class Student(models.Model):
     name = models.CharField(max_length=100)
 
+    def __unicode__(self):
+        return self.name
+
 class Course(models.Model):
     name = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.name
