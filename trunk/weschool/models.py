dcbd2477d5ad7e57e357cbde805f873d1cb84659
orn@skynvilla.is <orn@skynvilla.is@941bb81a-f064-d633-28c5-ba8e112e393c>
2012-02-06 21:59:25 +0000

    beinagrind af módeli

--- trunk/weschool/models.py
+++ trunk/weschool/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
-# Create your models here.
 class Exam(models.Model):
     question = models.CharField(max_length=100)
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    course = models.ForeignKey('Course')
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
