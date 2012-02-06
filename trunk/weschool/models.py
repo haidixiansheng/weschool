257adc2e6288d663cce8a72a5179134b662f1e8a
orn@skynvilla.is <orn@skynvilla.is@941bb81a-f064-d633-28c5-ba8e112e393c>
2012-02-06 23:41:04 +0000

    smá viðbætur

--- trunk/weschool/models.py
+++ trunk/weschool/models.py
@@ -1,25 +1,25 @@
 from django.db import models
 
 class Exam(models.Model):
+    title = models.CharField(max_length=200)
     question = models.CharField(max_length=100)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
 
     def __unicode__(self):
-        return self.question
+        return self.title
 
 class Teacher(models.Model):
     name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
     course = models.ForeignKey('Course')
-    GENDER_CHOICES = (
-        ('M', 'Male'),
-        ('F', 'Female'),
-    )
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
 
     def __unicode__(self):
         return self.name
 
 class Student(models.Model):
     name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
 
     def __unicode__(self):
         return self.name
