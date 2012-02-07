c3e0f7939f4d47024f43a5e5f2fdc67247b635c2
orn@skynvilla.is <orn@skynvilla.is@941bb81a-f064-d633-28c5-ba8e112e393c>
2012-02-07 21:36:20 +0000

    bætti við question klasa

--- trunk/weschool/admin.py
+++ trunk/weschool/admin.py
@@ -1,12 +1,18 @@
-from models import Exam
-from models import Course
-from models import Student
-from models import Teacher
+from models import Exam, Course, Student, Teacher, Question
 from models import Question
 from django.contrib import admin
 
-admin.site.register(Exam)
-admin.site.register(Course)
+class ExamAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['course','title','question']}),
+        ('Date information', {'fields': ['start_date', 'end_date']}),
+    ]
+
+class CourseAdmin(admin.ModelAdmin):
+    fields = ['name']
+
+admin.site.register(Exam, ExamAdmin)
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Student)
 admin.site.register(Teacher)
 admin.site.register(Question)
