e9d7e7b9ab7056db56f45e1311c2e39120dd6cc0
orn@skynvilla.is <orn@skynvilla.is@941bb81a-f064-d633-28c5-ba8e112e393c>
2012-02-09 18:48:37 +0000

    breytti modeli fyrir question svo það sé ekki lengur manytomany

--- trunk/weschool/admin.py
+++ trunk/weschool/admin.py
@@ -2,14 +2,26 @@ from models import Exam, Course, Student, Teacher, Question
 from models import Question
 from django.contrib import admin
 
+class ChoiceInline(admin.TabularInline):
+    model = Question
+    extra = 3
+
+
 class ExamAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,               {'fields': ['course','title','question']}),
-        ('Date information', {'fields': ['start_date', 'end_date']}),
+        (None, {'fields': ['course', 'title']}),
+        ('Date information', {'fields': ['start_date', 'end_date'], 'classes': ['collapse']}),
     ]
+    inlines = [ChoiceInline]
+    list_display = ('course', 'title', 'start_date', 'end_date')
+    list_filter = ['start_date']
+    search_fields = ['question']
+    date_hierarchy = 'start_date'
+
 
 class CourseAdmin(admin.ModelAdmin):
     fields = ['name']
+
 
 admin.site.register(Exam, ExamAdmin)
 admin.site.register(Course, CourseAdmin)
