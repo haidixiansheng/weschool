0f1954dede40b27d913c87819f300477b624c329
orn <orn@skynvilla.is>
2012-02-09 19:32:25 +0000

    model breytingar. bætti við choice

--- weschool/admin.py
+++ weschool/admin.py
@@ -1,16 +1,26 @@
-from models import Exam, Course, Student, Teacher, Question
+from models import Exam, Course, Question, Choice
 from models import Question
 from django.contrib import admin
 
 class ChoiceInline(admin.TabularInline):
-    model = Question
+    model = Choice
     extra = 3
 
 
 class ExamAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['course', 'title']}),
-        ('Date information', {'fields': ['start_date', 'end_date'], 'classes': ['collapse']}),
+        ('Date information', {'fields': ['start_date', 'end_date']}),
+    ]
+    list_display = ('course', 'title', 'start_date', 'end_date')
+    list_filter = ['start_date']
+    search_fields = ['question']
+    date_hierarchy = 'start_date'
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+
     ]
     inlines = [ChoiceInline]
     list_display = ('course', 'title', 'start_date', 'end_date')
@@ -20,11 +30,10 @@ class ExamAdmin(admin.ModelAdmin):
 
 
 class CourseAdmin(admin.ModelAdmin):
-    fields = ['name']
+    fields = ['name', 'teacher']
 
 
 admin.site.register(Exam, ExamAdmin)
 admin.site.register(Course, CourseAdmin)
-admin.site.register(Student)
-admin.site.register(Teacher)
 admin.site.register(Question)
+admin.site.register(Choice)
--- weschool/models.py
+++ weschool/models.py
@@ -1,42 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Exam(models.Model):
     title = models.CharField(max_length=200)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
     course = models.ForeignKey('Course')
-#    question = models.ForeignKey('Question')
 
     def __unicode__(self):
         return self.title
 
+
+class Score(models.Model):
+    student = models.ForeignKey(User)
+    score = models.DecimalField(decimal_places=2, max_digits=4)
+    exam = models.ForeignKey('Exam')
+
+
 class Question(models.Model):
-#    content = models.CharField(max_length=200)
-    answer = models.CharField(max_length=200)
-    correct = models.BooleanField()
+    question = models.CharField(max_length=200)
     exam = models.ForeignKey('Exam')
 
     def __unicode__(self):
-        return self.answer
+        return self.question
 
-class Teacher(models.Model):
-    name = models.CharField(max_length=100)
-    username = models.CharField(max_length=100)
-    course = models.ForeignKey('Course')
 
-    def __unicode__(self):
-        return self.name
+class Choice(models.Model):
+    answer = models.CharField(max_length=200)
+    correct = models.BooleanField()
+    question = models.ForeignKey('Question')
 
-class Student(models.Model):
-    name = models.CharField(max_length=100)
-    username = models.CharField(max_length=100)
-    course = models.ForeignKey('Course')
-
-    def __unicode__(self):
-        return self.name
 
 class Course(models.Model):
     name = models.CharField(max_length=100)
+    teacher = models.ForeignKey(User)
 
     def __unicode__(self):
         return self.name
