9db9a241f808acfa570b24598e7f6066a7100430
davidhalldor <davidhalldor@941bb81a-f064-d633-28c5-ba8e112e393c>
2012-02-09 22:34:13 +0000

    New changelist

--- trunk/weschool/views.py
+++ trunk/weschool/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render_to_response
-from weschool.models import Course, Exam
+from weschool.models import Course, Exam, Choice, Question
 from django.http import Http404
 
 def index(request):
@@ -9,14 +9,18 @@ def index(request):
 def detail(request, course_id):
     try:
         course = Course.objects.get(id=course_id)
-        p = Exam.objects.filter(course=course_id)
+        exams = course.exam_set.all()
     except Course.DoesNotExist:
         raise Http404
-    return render_to_response('course_detail.html', {'exams': p, 'course' : course})
+    return render_to_response('course_detail.html', {'exams': exams, 'course' : course})
 
 def action(request, exam_id):
     try:
         exam = Exam.objects.get(id=exam_id)
+        questions = exam.question_set.all()
+        questions_list = []
+        for question in questions:
+            questions_list.append((question, question.choice_set.all()))
     except Course.DoesNotExist:
         raise Http404
-    return render_to_response('exam_action.htm', {'exam': exam })
+    return render_to_response('exam_action.html', {'exam': exam, 'questions_list' : questions_list })
