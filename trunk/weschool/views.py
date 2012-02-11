ffabb5e94046d40a2d65e47a2d53b32c7ccdc472
orn@skynvilla.is <orn@skynvilla.is@941bb81a-f064-d633-28c5-ba8e112e393c>
2012-02-11 01:09:55 +0000

    smá css æfingar. bætti við RequestContext sem leyfir css. Þarf að breyta kóða og taka út beina vísun í user í view.py

--- trunk/weschool/views.py
+++ trunk/weschool/views.py
@@ -7,7 +7,7 @@ from django.template import RequestContext
 
 def index(request):
     course_list = Course.objects.all()
-    return render_to_response('course_index.html', {'course_list': course_list, 'user': request.user})
+    return render_to_response('course_index.html', {'course_list': course_list, 'user': request.user}, RequestContext(request))
 
 def detail(request, course_id):
     try:
@@ -15,7 +15,7 @@ def detail(request, course_id):
         exams = course.exam_set.all()
     except Course.DoesNotExist:
         raise Http404
-    return render_to_response('course_detail.html', {'exams': exams, 'course' : course, 'user': request.user})
+    return render_to_response('course_detail.html', {'exams': exams, 'course' : course, 'user': request.user}, RequestContext(request))
 
 def action(request, exam_id):
 
@@ -27,10 +27,10 @@ def action(request, exam_id):
             questions_list.append((question, question.choice_set.all()))
     except Course.DoesNotExist:
         raise Http404
-    return render_to_response('exam_action.html', {'exam': exam, 'questions_list' : questions_list, 'user': request.user })
+    return render_to_response('exam_action.html', {'exam': exam, 'questions_list' : questions_list, 'user': request.user }, RequestContext(request))
 
 def home_index(request):
-    return render_to_response('home.html', {'next': request, 'user': request.user})
+    return render_to_response('home.html', {'next': request, 'user': request.user}, RequestContext(request))
 
 def logout_page(request):
     logout(request)
